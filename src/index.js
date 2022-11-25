9d6f12d89bffba728aee7ccda0e644267e337afa
Yurii Voloshyn <voloshyn9900@gmail.com>
2022-11-25 19:33:13 +0100

    commit2

--- src/index.js
+++ src/index.js
@@ -45,3 +45,30 @@ let select = function () {
 
 
 select();
+
+
+
+// slider Voloshyn
+let offset = 0;
+const sliderLine = document.querySelector('.slider__line');
+
+document.querySelector('.button__slide--1').addEventListener('click', function () {
+    offset = 0;
+    sliderLine.style.right = offset + 'px';
+    
+
+});
+
+document.querySelector('.button__slide--2').addEventListener('click', function () {
+    offset = 1280;
+    sliderLine.style.right = offset + 'px';
+
+
+});
+
+document.querySelector('.button__slide--3').addEventListener('click', function () {
+    offset = 2560;
+    sliderLine.style.right = offset + 'px';
+
+
+});
