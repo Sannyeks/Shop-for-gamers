614146d5c3ffba0c5627ac1ae4beace8752d4c28
Alekstitula84 <alekstitula84@gmail.com>
2022-11-26 15:49:16 +0200

    переробив слайдер

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,56 +1,12 @@
-let offset = 0;
-const sliderLine = document.querySelector('.slider__line');
 
-document.querySelector('.button__slide--1').addEventListener('click', function () {
-    offset = 0;
-    sliderLine.style.right = offset + 'px';
-    
-
-});
-
-document.querySelector('.button__slide--2').addEventListener('click', function () {
-    offset = 1280;
-    sliderLine.style.right = offset + 'px';
-
-
-});
-
-document.querySelector('.button__slide--3').addEventListener('click', function () {
-    offset = 2560;
-    sliderLine.style.right = offset + 'px';
-
-
-});
-
-
-// (() => {
-//   const refs = {
-//     openModalBtn: document.querySelector('[data-modal-open]'),
-//     closeModalBtn: document.querySelector('[data-modal-close]'),
-//     modal: document.querySelector('[data-modal]'),
-//   };
-
-//   refs.openModalBtn.addEventListener('click', toggleModal);
-//   refs.closeModalBtn.addEventListener('click', toggleModal);
-
-//   function toggleModal() {
-//     refs.modal.classList.toggle('is-hidden');
-//   }
-// })();
-
-
-
-
-
-// let offset = 0;
-// const sliderLine = document.querySelector('.slider__line');
-
-// document.querySelector('.button__slide--1').addEventListener('click', function () {
-//     offset = offset + 1280;
-//     if (offset > 2480) {
-//         offset = 0;
-//     }
-//     sliderLine.style.right = offset + 'px';
-
-
-// });
+var swiper = new Swiper (".mySwiper", {
+        slidesPerView: 1,
+        loop: true,
+        clickable: true,
+        pagination: {
+          el: ".swiper-pagination",
+          dynamicBullets: true,
+  },
+          
+        autoplay: { delay: 8000 },
+      });
