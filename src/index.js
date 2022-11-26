efb979f859e81dabc472ec74e1c607a68eaf9888
Alekstitula84 <alekstitula84@gmail.com>
2022-11-26 19:20:47 +0200

    slider

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 
 import './js/menu';
-import './js/slider';
 import './js/menu-list';
 import './js/modal-buy';
 import './js/modal-cart';
 import './js/modal-send';
+import './js/slider';
 
--- src/js/slider.js
+++ src/js/slider.js
@@ -1,12 +1,27 @@
 
-var swiper = new Swiper (".mySwiper", {
-        slidesPerView: 1,
-        loop: true,
-        clickable: true,
+var swiper = new Swiper(".mySwiper", {
+        effect: "cube",
+        grabCursor: true,
+        cubeEffect: {
+          shadow: true,
+          slideShadows: true,
+          shadowOffset: 20,
+          shadowScale: 0.94,
+        },
         pagination: {
           el: ".swiper-pagination",
-          dynamicBullets: true,
-  },
-          
-        autoplay: { delay: 8000 },
+        },
       });
+  
+//   {
+//   slidesPerView: 1,
+//   loop: true,
+//   clickable: true,
+// },
+//         pagination: {
+//           el: ".swiper-pagination"
+//           // dynamicBullets: true,
+//   },
+
+//         autoplay: { delay: 8000 },
+//       );
